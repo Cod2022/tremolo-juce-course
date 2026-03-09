23df848c6f40325f4c799107b0ad9584f0af64ec
Denis Shchevelev <you@example.com>
2026-03-10 02:45:03 +0300

    created lfo generator in Tremolo.h

--- todo/tremolo_plugin/include/Tremolo/Tremolo.h
+++ todo/tremolo_plugin/include/Tremolo/Tremolo.h
@@ -3,14 +3,31 @@
 namespace tremolo {
 class Tremolo {
 public:
+  Tremolo() {
+	// We can temporarily set frequency to 440Hz just to be able to hear the output of our LFO
+	// But for actual LFO we need the range between 1 and 20 HZ
+	lfo.setFrequency(5.f, true);
+  }
   void prepare(double sampleRate, int expectedMaxFramesPerBlock) {
-    juce::ignoreUnused(sampleRate, expectedMaxFramesPerBlock);
+
+	// This is the Structure containing info about sampleRate, BlockSize and numChannels
+	// Many functions of juce_dsp classes take this structure as an argument
+	const juce::dsp::ProcessSpec processSpec {
+		.sampleRate = sampleRate,
+		.maximumBlockSize = static_cast<juce::uint32>(expectedMaxFramesPerBlock),
+		.numChannels = 1u,
+	};
+	// Like this one for example
+	lfo.prepare(processSpec);
+
+
   }
 
   void process(juce::AudioBuffer<float>& buffer) noexcept {
     // for each frame
     for (const auto frameIndex : std::views::iota(0, buffer.getNumSamples())) {
-      // TODO: generate the LFO value
+      // generate the LFO value
+	  const auto lfoValue = lfo.processSample(0.f); 
 
       // TODO: calculate the modulation value
 
@@ -20,8 +37,9 @@ public:
         // get the input sample
         const auto inputSample = buffer.getSample(channelIndex, frameIndex);
 
-        // TODO: modulate the sample
-        const auto outputSample = inputSample;
+        // Checking our lfoValuue just by outputing it instead of the original input signal
+		// So we should hear its sound instead of that sended to our plugin
+        const auto outputSample = 0.1f * lfoValue;
 
         // set the output sample
         buffer.setSample(channelIndex, frameIndex, outputSample);
@@ -29,9 +47,15 @@ public:
     }
   }
 
-  void reset() noexcept {}
+  void reset() noexcept {
+	// Releasing our lfo resources
+	lfo.reset();
+  }
 
 private:
   // You should put class members and private functions here
+  
+  // Creating an Oscillator class instance for our LFO
+  juce::dsp::Oscillator<float> lfo{ [](auto phase){ return std::sin(phase); }};
 };
 }  // namespace tremolo
