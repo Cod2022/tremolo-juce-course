142e7390201cbbd38c9c64ed384fcc28c8ba9c38
Denis Shchevelev <you@example.com>
2026-05-05 21:47:33 +0300

    started implementing SerializableParameters

--- todo/tremolo_plugin/source/JsonSerializer.cpp
+++ todo/tremolo_plugin/source/JsonSerializer.cpp
@@ -1,9 +1,25 @@
 namespace tremolo {
+namespace {
+struct SerializableParameters {
+	float rate;
+	bool bypassed;
+	juce::String waveform;
+};
+
+SerializableParameters from(const tremolo::Parameters& parameters) {
+	return {
+		.rate = parameters.rate.get(),
+		.bypassed = parameters.bypassed.get(),
+		.waveform = parameters.waveform.getCurrentChoiceName(),
+	};
+}
+}
 void JsonSerializer::serialize(const Parameters& parameters,
                                juce::OutputStream& output) {
-  juce::ignoreUnused(parameters, output);
 
-  // TODO: serialize parameters to the output stream as JSON
+  // serialize parameters to the output stream as JSON
+	const auto parametersToSerialize = from(parameters);
+	
 }
 
 juce::Result JsonSerializer::deserialize(juce::InputStream& input,
