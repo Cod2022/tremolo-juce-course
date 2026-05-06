20fa364238ceb3ab75c4dcd399ea6a094f7fcc33
Denis Shchevelev <you@example.com>
2026-05-07 00:28:43 +0300

    implemented serialization feature (without deserialization yet)

--- todo/tremolo_plugin/source/JsonSerializer.cpp
+++ todo/tremolo_plugin/source/JsonSerializer.cpp
@@ -4,6 +4,23 @@ struct SerializableParameters {
 	float rate;
 	bool bypassed;
 	juce::String waveform;
+	// TODO: add gain parameter to serialize
+
+	static constexpr auto marshallingVersion = 1;
+
+	template <typename Archive, typename T>
+	static void serialise(Archive& archive, T& t) {
+		using namespace juce;
+		if (archive.getVersion() != 1) {
+			return;
+		}
+
+		// TREMOLO_PLUGIN_NAME is a preprocessor definition specified in the CMakeLists.txt
+		archive(named("pluginName", TREMOLO_PLUGIN_NAME));
+		archive(named("modulationRateHz", t.rate), named("bypassed", t.bypassed),
+			named("modulationWaveform", t.waveform));
+	
+	}
 };
 
 SerializableParameters from(const tremolo::Parameters& parameters) {
@@ -17,8 +34,18 @@ SerializableParameters from(const tremolo::Parameters& parameters) {
 void JsonSerializer::serialize(const Parameters& parameters,
                                juce::OutputStream& output) {
 
-  // serialize parameters to the output stream as JSON
+    // serialize parameters to the output stream as JSON
 	const auto parametersToSerialize = from(parameters);
+	const auto json = juce::ToVar::convert(parametersToSerialize);
+	// abort serialization if the convertion fails (json does not have value)
+	if (!json.has_value()) {
+		return;
+	}
+
+	juce::JSON::writeToStream(output, *json, juce::JSON::FormatOptions{}
+								.withSpacing(juce::JSON::Spacing::multiLine)
+								.withMaxDecimalPlaces(2));
+
 	
 }
 
